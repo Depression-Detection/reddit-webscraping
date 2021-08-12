ec0376b7079f97d93ca452b423aa92d076dfb65e
Rithwik Babu <rithwikbabu13@gmail.com>
2021-08-12 15:52:06 -0500

    Fixed paths to accomdate for organization changes

--- store-users.py
+++ store-users.py
@@ -1,22 +1,22 @@
 import csv
 
-with open('users.txt') as f:
+with open('data/users.txt') as f:
     list = []
     for line in f:
         inner_list = [elt.strip() for elt in line.split('/t')]
         list.append(inner_list)
 
-with open("users.csv", "w", newline="") as f:
+with open("data/users.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(list)
 
-with open('users.csv') as f:
+with open('data/users.csv') as f:
     list = []
     for line in f:
         inner_list = [elt.strip() for elt in line.split(' ')]
         list.append(inner_list)
 
-with open("users.csv", "w", newline="") as f:
+with open("data/users.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(list)
 
